50cc6ec3cf91b22cc30f3e18cce3c657b06ec74c
JuniorD-Isael <isaeldjunior@gmail.com>
2025-02-02 17:33:49 -0300

    Adiciona suporte ao acesso de postagens por slug e cria ações para listar e visualizar postagens

--- techsolutions_blog/base/views.py
+++ techsolutions_blog/base/views.py
@@ -3,11 +3,14 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 from .models import Postagem
 from .serializers import PostagemSerializer
+from rest_framework.decorators import action
+
 
 class PostagemViewSet(viewsets.ModelViewSet):
     queryset = Postagem.objects.all()
     serializer_class = PostagemSerializer
     permission_classes = [IsAuthenticated]
+    lookup_field = 'slug'
 
     def perform_create(self, serializer):
         serializer.save(autor=self.request.user)
@@ -23,3 +26,15 @@ class PostagemViewSet(viewsets.ModelViewSet):
         if postagem.autor != self.request.user:
             return Response({"detail": "Você não tem permissão para editar esta postagem."}, status=403)
         serializer.save(autor=self.request.user)
+
+    @action(detail=False, methods=['get'])
+    def lista_postagens(self, request):
+        postagens = self.queryset.all()
+        serializer = self.get_serializer(postagens, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['get'])
+    def visualizar_postagem(self, request, pk=None):
+        postagem = self.get_object()
+        serializer = self.get_serializer(postagem)
+        return Response(serializer.data)
