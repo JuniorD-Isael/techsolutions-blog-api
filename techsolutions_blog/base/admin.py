53d88ee3dc9598b8f1e5d7fec3007b578c52305e
JuniorD-Isael <isaeldjunior@gmail.com>
2025-02-02 13:53:33 -0300

    feat: configurar app base e ajustes iniciais
    - Adiciona o app `base` ao projeto
    - Configura o modelo de postagem
    - Registra o modelo no admin
    - Adiciona dependências ao `requirements.txt`
    - Aplica migrações iniciais

--- techsolutions_blog/base/admin.py
+++ techsolutions_blog/base/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
+from pyclbr import Class
 
-# Register your models here.
+from django.contrib import admin
+from .models import Postagem
+
+@admin.register(Postagem)
+class PostagemAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor','data_postagem')
+    search_fields = ('titulo', 'autor_username')
+    list_filter = ('data_postagem', 'autor')
--- techsolutions_blog/base/models.py
+++ techsolutions_blog/base/models.py
@@ -1,3 +1,24 @@
+import uuid
+
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 # Create your models here.
+
+class Postagem(models.Model):
+    titulo = models.CharField(max_length=100)
+    data_postagem = models.DateTimeField(auto_now_add=True)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE, related_name="postagens")
+    imagem = models.ImageField(upload_to='postagens', blank=True, null=True)
+    artigo = models.TextField(max_length=4000)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = f"{slugify(self.titulo)}-{uuid.uuid4()}"
+        super(Postagem, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.titulo
