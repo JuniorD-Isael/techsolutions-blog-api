1586f3d8a7919771e86321ed8e9e8805f60bf544
JuniorD-Isael <isaeldjunior@gmail.com>
2025-02-03 21:14:07 -0300

    implementa autenticação JWT para a API

--- techsolutions_blog/urls.py
+++ techsolutions_blog/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 from .base.views import PostagemViewSet
 from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'postagens', PostagemViewSet, basename='postagem')
 
 urlpatterns = [
-    path('admin', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('api/token/', obtain_auth_token, name='api_token_auth'),
+    path("admin", admin.site.urls),
+    path("api/", include(router.urls)),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
