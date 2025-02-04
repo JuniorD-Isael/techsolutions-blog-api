a82f91bc1ebbe09d95a7eed8d577d0028dd5e9ab
JuniorD-Isael <isaeldjunior@gmail.com>
2025-02-03 21:56:14 -0300

    adiciona documentação da API usando Swagger

--- techsolutions_blog/settings.py
+++ techsolutions_blog/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'techsolutions_blog.base',
     'rest_framework',
     'rest_framework.authtoken',
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -113,6 +114,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 
--- techsolutions_blog/urls.py
+++ techsolutions_blog/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .base.views import PostagemViewSet
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 router = DefaultRouter()
 router.register(r'postagens', PostagemViewSet, basename='postagem')
@@ -12,4 +13,8 @@ urlpatterns = [
     path("api/", include(router.urls)),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/",SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
